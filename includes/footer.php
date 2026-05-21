b2629782c71cb2d10b1c778aa327378f351c761d
Joao Vitor <joao.rodrigues@baraodemaua.br>
2026-05-21 15:32:28 -0300

    ajustado width do footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,14 @@
 <footer>
 
-  <div class="footer-content" >
+  <div class="footer-content">
     <div>
       <div class="footer-brand">Dr. <span>Daniel</span></div>
       <div class="lgpd-note"><span data-lang="pt">© 2026 · Todos os direitos reservados · LGPD</span><span
           data-lang="en">© 2026 · All rights reserved · LGPD</span></div>
     </div>
-    <div class="footer-copy">CRP 06/130646</div>
+    <div class="footer-copy">
+      CRP 06/130646
+    </div>
   </div>
 
 </footer>
